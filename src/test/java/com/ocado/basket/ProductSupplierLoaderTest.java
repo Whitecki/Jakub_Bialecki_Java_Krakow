0f8067a2813d31b6b8a2598039b0e379ae78368e
Jakub Białecki <jakubialecki@student.agh.edu.pl>
2024-04-08 05:08:07 +0200

    jar

--- src/test/java/com/ocado/basket/ProductSupplierLoaderTest.java
+++ src/test/java/com/ocado/basket/ProductSupplierLoaderTest.java
@@ -13,7 +13,6 @@ class ProductSupplierLoaderTest {
 
     @Test
     void testReadJsonFile() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, IOException {
-        // Przygotowanie
 
         String filePath = "C:\\Users\\elyjk\\Desktop\\studia\\Poszukiwanie Sta\u017Cu\\Ocado\\Jakub_Bialecki_Java_Krakow\\src\\main\\resources\\config.json";
 
@@ -21,18 +20,14 @@ class ProductSupplierLoaderTest {
         Method method = ProductSupplierLoader.class.getDeclaredMethod("readJsonFile", String.class);
         method.setAccessible(true);
 
-        // Działanie
         Object response = method.invoke(reader, filePath);
 
         assertInstanceOf(Map.class, response);
-        @SuppressWarnings("unchecked") // Dodajemy to, aby stłumić ostrzeżenie o niezweryfikowanym rzutowaniu
         Map<String, List<String>> result;
         if (response instanceof Map<?, ?>) {
             result = (Map<String, List<String>>) response;
-            // Weryfikacja
             assertNotNull(result);
             assertFalse(result.isEmpty());
-            // Dodaj więcej asercji, aby sprawdzić szczegółowe dane zwrócone z pliku JSON
         } else {
             fail("Metoda readJsonFile nie zwróciła Map<String, List<String>>");
         }
